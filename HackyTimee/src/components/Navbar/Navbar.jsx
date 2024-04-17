6973f6e0c06faed7ad3e82fd5aa4cf6d1119086e
mikeAdelek <oikeoluwa@gmail.com>
2024-04-18 00:05:41 +0100

    Initial commit

---
+++ HackyTimee/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import Menu from "../../assets/images/nav-menu.png";
+import Close from "../../assets/images/Vectorcancel.png";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [toggle, settoggle] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <div className="app__navbar">
+      <div className="nav_logo">
+        <Link className="link" to="/">
+          <h1>
+            Hacky<span className="Time">Timee</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="nav__links">
+        <ul>
+          <Link to="#timeline" className="link">
+            <li>Timeline</li>
+          </Link>
+          <Link className="link">
+            <li>Overview</li>
+          </Link>
+          <Link to="#faqs" className="link">
+            <li>FAQs</li>
+          </Link>
+          <Link className="link" to="/contact">
+            <li>Contact</li>
+          </Link>
+        </ul>
+        <Link className="link" to="/register">
+          <button
+            onClick={handleClick}
+            className={`gradient btn ${clicked ? "clicked" : ""}`}
+          >
+            Register
+          </button>
+        </Link>
+      </div>
+
+      {/* Mobile Menu  */}
+      <div className="app-mobile-menu">
+        <img
+          className="menu"
+          src={Menu}
+          onClick={() => settoggle((prev) => !prev)}
+        />
+        {toggle && (
+          <div className="mobile-menu slide-bottom slide-top">
+            <img src={Close} onClick={() => settoggle((prev) => !prev)} />
+            <ul>
+              <Link to="#timeline" className="link">
+                <li onClick={() => settoggle((prev) => !prev)}>Timeline</li>
+              </Link>
+              <Link className="link">
+                <li onClick={() => settoggle((prev) => !prev)}>Overview</li>
+              </Link>
+              <Link to="#faqs" className="link">
+                <li onClick={() => settoggle((prev) => !prev)}>FAQs</li>
+              </Link>
+              <Link className="link" to="/contact">
+                <li onClick={() => settoggle((prev) => !prev)}>Contact</li>
+              </Link>
+            </ul>
+            <Link className="link" to="/register">
+              <button
+                onClick={() => settoggle((prev) => !prev)}
+                className="gradient btn"
+              >
+                Register
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ HackyTimee/src/components/Privacy/Privacy.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./Privacy.css";
+import HeaderText from "../HeaderText/HeaderText";
+import Unlocked from "../../assets/images/unlock.png";
+import Tick from "../../assets/images/tick.png";
+import Flare1 from "../../assets/images/flare1.png";
+import bPink from "../../assets/images/bigPink.png";
+import sPink from "../../assets/images/smallPink.png";
+import sGray from "../../assets/images/stargray1.png";
+import sGray2 from "../../assets/images/stargray2.png";
+import sTiny from "../../assets/images/startiny.png";
+import sWhite from "../../assets/images/starwhite.png";
+
+const Privacy = () => {
+  return (
+    <div className="app__privacy">
+      <img className="fpr1" src={Flare1} />
+      <div className="privacy-inner privacy-text">
+        <HeaderText text="Privacy Policy and" colored="Terms" />
+        <p className="date">Last update March 28, 2010</p>
+        <p className="policy">
+          Below are our privacy & policy, which outline a lot of goodies. it’s
+          our aim to always take of our participant
+        </p>
+        <img src={sPink} className="blink priv-s1" />
+        <img src={sGray} className="blink priv-s2" />
+        <img src={sPink} className="blink priv-s3" />
+        <img src={sTiny} className="blink priv-s4" />
+        <img src={sPink} className="blink priv-s5" />
+        <img src={sWhite} className="blink priv-s6" />
+        <img src={sGray2} className="blink priv-s7" />
+        <div className="privacy-box">
+          <p>
+            At HackyTime tech Hackathon 1.0, we value your privacy and are
+            committed to protecting your personal information. This Privacy
+            Policy outlines how we collect, use, disclose, and safeguard your
+            data when you participate in our tech hackathon event. By
+            participating in our event, you consent to the practices described
+            in this policy.
+          </p>
+          <div>
+            <span className="hterm">
+              <p className="pink">Listening Policy</p>
+              <p className="h-term">Here are terms of our Standard License:</p>
+            </span>
+            <span className="ticks">
+              <img src={Tick} />
+              <p>
+                The Standard License grants you a non-exclusive right to
+                navigate and register for our event
+              </p>
+            </span>
+            <span className="ticks">
+              <img src={Tick} />
+              <p>
+                You are licensed to use the item available at any free source
+                sites, for your project development
+              </p>
+            </span>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          >
+            <button className="btn gradient">Read More</button>
+          </div>
+        </div>
+      </div>
+      <div className="privacy-inner privacy-img">
+        {/* <img className="image3" src={Locked} alt="" /> */}
+        <img className="image4" src={Unlocked} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Privacy;
